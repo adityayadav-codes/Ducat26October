bae4415f9d348f21a578da7f21ae9b1ef20f2244
adityayadav-codes <adityayadav2905@gmail.com>
2025-10-26 12:44:43 +0530

    add demo.java & deploy today first program

---
+++ Demo1.java
@@ -0,0 +1,18 @@
+public class Demo1 	{
+
+	public static void main(String args[] )	{
+	int num =-10;
+
+	if(num >=0)	{
+
+		System.out.println("Postive Number!!");
+
+	}
+	else 	{
+
+	System.out.println("Negative number!");
+
+        
+     }   
+    }
+}
