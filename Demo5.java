79c18f97396f8a959102362472b1ab99251a316b
adityayadav-codes <adityayadav2905@gmail.com>
2025-10-26 13:24:09 +0530

    this program to check number is greater than 10 or not

---
+++ Demo5.java
@@ -0,0 +1,13 @@
+public class Demo5 {
+	public static void main(String args []){
+
+	int n = 2;
+	
+	if(n >10)	{
+	
+		System.out.println("Number is Greater than 10");
+	}	else {
+		System.out.println("Number is Less than 10");
+	}
+}
+}
