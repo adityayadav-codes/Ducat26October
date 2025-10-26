0bee838e5c2d5d83787aa50197215fb9ee88fa45
adityayadav-codes <adityayadav2905@gmail.com>
2025-10-26 13:17:52 +0530

    This program check to a string empty or not

---
+++ Demo4.java
@@ -0,0 +1,13 @@
+public class Demo4 {
+		public static void main(String args [] )	{
+	
+		String str ="";
+	if (str == "")	{
+
+		System.out.println("empty string");
+}	else {
+		
+		System.out.println("String is not empty");
+	}
+}
+}
