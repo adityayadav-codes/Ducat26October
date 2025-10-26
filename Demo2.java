1be6a0e2499031529f968efa20c06e00d2c4fd75
adityayadav-codes <adityayadav2905@gmail.com>
2025-10-26 12:54:57 +0530

    I wrote a java program for a person is eligible to vote based on their age

---
+++ Demo2.java
@@ -0,0 +1,19 @@
+public class Demo2	{
+
+	public static void main (String args [] )	{
+
+		int age = 18;
+		
+			if (age >=18)	{
+	
+				System.out.println("You are eligible  for voting");
+
+		} else if ( age == 17)		{
+	
+				System.out.println("You are not eligible  for voting, Please wait for 1 year!");	 
+		}	else 	{
+			
+				System.out.println("You are not eligible");
+			}
+}	
+}
