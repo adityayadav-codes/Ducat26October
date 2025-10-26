1fe3a2723a4b13216508dd02ed1a231f14fba23c
adityayadav-codes <adityayadav2905@gmail.com>
2025-10-26 13:06:00 +0530

    this program to check if a no is even or odd

---
+++ Demo3.java
@@ -0,0 +1,14 @@
+public class Demo3	{
+	public static void main (String args [])	{
+
+	int n = 10;
+	 
+ 	if (n % 2 == 0)	{
+	
+		System.out.println("Even Number!");	
+}	else     {
+    
+        System.out.println("Odd Number!");
+    }
+}
+}
